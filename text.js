1bdab5e1c8137b590b44836133878256e0461038
Bob <blund@cablelabs.com>
2022-10-11 14:38:44 -0600

    initial commit

---
+++ text.js
@@ -0,0 +1,365 @@
+'use strict';
+
+// UI
+const callToUsernameInput = document.querySelector('#callToUsernameInput');
+const chatBtn = document.querySelector('#chatBtn');
+const videoBtn = document.querySelector('#videoBtn');
+const fileBtn = document.querySelector('#fileBtn');
+const hangUpBtn = document.querySelector('#hangUpBtn');
+const brumeBtn = document.querySelector('#brumeBtn');
+const msgInput = document.querySelector('#msgInput');
+const sendMsgBtn = document.querySelector('#sendMsgBtn');
+const chatArea = document.querySelector('#chatarea');
+const chatInput = document.querySelector('#chatInput');
+
+chatBtn.disabled = true;
+videoBtn.disabled = true;
+fileBtn.disabled = true;
+sendMsgBtn.disabled = true;
+hangUpBtn.disabled = true;
+
+const iceServers = { 'iceServers': [
+	{ 'urls': 'stun:stun2.1.google.com:19302' }/*,
+	{ 'urls': 'stun:stun.stunprotocol.org:3478' },
+	{ 'urls': 'stun:stun.l.google.com:19302' },*/
+]};
+
+//var name; 
+var connectedUser;
+var peerConnection = null; 
+var dataChannel = null;
+var rtcMode = null;
+var remoteDescriptionSet = false;
+var iceBuffer = [];
+
+const ws  = (function() {
+	var wsServer = {};
+
+	const open = function () {
+		return new Promise((res, rej) => {
+			//https://websockets.readthedocs.io/en/stable/topics/authentication.html
+			wsServer = new WebSocket(`ws://localhost:3000?token=${localStorage.Authorization}`); 
+
+			wsServer.onopen = function () { 
+				console.log("Connected to the signaling server");
+				res();
+			};
+			
+			//when we got a message from a signaling server
+			wsServer.onmessage = function (msg) {  
+				var data = JSON.parse(msg.data);
+				data = {...data.data, name: data.from};
+				console.log(`Got message ${data.type} from ${data.name}`);
+				
+				switch(data.type) {   
+					case "offer": 
+						handleOffer(data.offer, data.name);
+						break;
+
+					case "answer": 
+						handleAnswer(data.answer); 
+						break;
+
+					case "candidate": 
+						handleCandidate(data.candidate); 
+						break;
+					default:
+						break; 
+				} 
+			};
+
+			wsServer.onerror = function (err) { 
+				//console.log("Got error", err);
+				rej(err);
+			};
+		});
+	};
+
+	//alias for sending JSON encoded messages 
+	const send = function(message) {
+		if (connectedUser) { 
+			wsServer.send(JSON.stringify({
+				action: 'send',
+				to: connectedUser,
+				data: {
+					...message
+				}
+			}));
+		}
+	};
+
+	const close = function() {
+		wsServer.close();
+	};
+
+	return {open, send, close};
+})();
+
+//connect to ws server
+brumeBtn.addEventListener("click", async function () {
+	if(brumeBtn.textContent == 'Brume Connect') {
+		try {
+			await ws.open();
+			chatBtn.disabled = false;
+			videoBtn.disabled = false;
+			fileBtn.disabled = false;
+			brumeBtn.textContent = 'Brume Disconnect';
+		} catch(e) {
+			alert(`Error connecting to Brume: ${e}`);
+		}
+	} else {
+		ws.close();
+		brumeBtn.textContent = 'Brume Connect';
+		chatBtn.disabled = true;
+		videoBtn.disabled = true;
+		fileBtn.disabled = true;
+	}
+});
+
+function dataChannelOnClose() {
+	console.log("data channel is closed");
+	peerConnection = null;
+	dataChannel = null;
+	chatArea.innerHTML = '';
+	hangUpBtn.disabled = true;
+	sendMsgBtn.disabled = true;
+	chatBtn.disabled = false;
+	chatArea.style.display = 'none';
+	chatInput.style.display = 'none';
+}
+
+/*
+*	https://webrtc.org/getting-started/overview
+*/
+
+// Initiate chat
+chatBtn.addEventListener("click", function () {
+	peerConnection = new RTCPeerConnection(iceServers); 
+	peerConnection.onicecandidate = function (event) { 
+		if (event.candidate) { 
+			ws.send({ type: "candidate", candidate: event.candidate }); 
+		} 
+	}; 
+   
+	if (callToUsernameInput.value.length > 0) { 
+		connectedUser = callToUsernameInput.value;
+		dataChannel = peerConnection.createDataChannel("channel1", {reliable:true});
+
+		//when we receive a message from the other peer, display it on the screen 
+		dataChannel.onmessage = function (event) { 
+			chatArea.innerHTML += connectedUser + ": " + event.data + "<br />"; 
+			chatArea.scrollTop = chatArea.scrollHeight;
+		};
+
+		dataChannel.onerror = function (error) { 
+			console.log("Ooops...error:", error); 
+		 };
+
+		 dataChannel.onclose = dataChannelOnClose;
+		// create an offer 
+		peerConnection.createOffer(async function (offer) {
+			offer.app = 'text';
+			ws.send({ type: "offer", offer: offer }); 
+			await peerConnection.setLocalDescription(offer);
+			console.log('createOffer setLocalDescription'); 
+		}, function (error) { 
+			alert("Error when creating an offer", error); 
+		});
+
+		rtcMode = 'chat';
+		hangUpBtn.disabled = false;
+		chatBtn.disabled = true;
+		chatArea.style.display = '';
+		chatInput.style.display = '';
+	} 
+   
+});
+
+// Initiate Video
+videoBtn.addEventListener("click", async function () {
+	let localStream; //74
+	try {
+		localStream = await navigator.mediaDevices.getUserMedia({
+			video: true,
+			audio: true
+		});
+	} catch (error) {
+		alert(`${error.name}`);
+		console.error(error);
+	}
+	//86
+	document.querySelector('video#local').srcObject = localStream;
+
+	peerConnection = new RTCPeerConnection(iceServers);
+	peerConnection.addStream(localStream);
+	peerConnection.onaddstream = event => {
+		document.querySelector('video#remote').srcObject = event.stream;
+	};
+
+	peerConnection.onicecandidate = function (event) { 
+		if (event.candidate) { 
+			ws.send({ type: "candidate", candidate: event.candidate }); 
+		} 
+	}; 
+   
+	if (callToUsernameInput.value.length > 0) { 
+		connectedUser = callToUsernameInput.value;
+		peerConnection.createOffer(async function (offer) {
+			offer.app = 'av';
+			ws.send({ type: "offer", offer: offer }); 
+			await peerConnection.setLocalDescription(offer);
+			console.log('createOffer setLocalDescription');
+		}, function (error) { 
+			alert("Error when creating an offer"); 
+		});
+
+		hangUpBtn.disabled = false;
+		videoBtn.disabled = true;
+	} 
+
+	rtcMode = 'video';
+   
+});
+
+// Hang up chat
+hangUpBtn.addEventListener("click", function () {  
+	connectedUser = null;
+	if(peerConnection) { 
+		peerConnection.close(); 
+		peerConnection.onicecandidate = null;
+		chatArea.style.display = 'none';
+		chatInput.style.display = 'none';
+	}
+});
+ 
+// Chat send
+sendMsgBtn.addEventListener("click", function (event) { 
+	var val = msgInput.value; 
+	chatArea.innerHTML += name + ": " + val + "<br />";
+	chatArea.scrollTop = chatArea.scrollHeight;
+   
+	//sending a message to a connected peer 
+	dataChannel.send(val); 
+	msgInput.value = ""; 
+});
+ 
+// Handle WebRTC offer
+async function handleOffer(offer, name) {
+	const offerMeda = offer.sdp
+		.split('\r\nm=').slice(1)
+		.map(e=>e.split('\r\n')[0].split(' ')[0]);
+
+	//93
+	peerConnection = new RTCPeerConnection(iceServers); 
+		
+	// 104 
+	peerConnection.onicecandidate = function (event) {
+		if (event.candidate) { 
+			ws.send({ type: "candidate", candidate: event.candidate }); 
+		} 
+	};
+
+	connectedUser = name; // 142
+	const appType = offer.app;
+	delete offer.app;
+	await peerConnection.setRemoteDescription(offer); //(new RTCSessionDescription(offer));
+	remoteDescriptionSet = true;
+	handleCandidate();
+	console.log('offer setRemoteDescription');
+  
+	switch(appType) {
+		case 'av':
+			let localStream; //74
+			try {
+				localStream = await navigator.mediaDevices.getUserMedia({
+					video: true,
+					audio: true
+				});
+			} catch (error) {
+				alert(`${error.name}`);
+				console.error(error);
+			}
+			// 86
+			document.querySelector('video#local').srcObject = localStream;
+			// 96
+			peerConnection.addStream(localStream);
+			// 99
+			peerConnection.onaddstream = event => {
+				document.querySelector('video#remote').srcObject = event.stream;
+			};
+			//143
+			//await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+			break;
+
+		case 'text':
+			peerConnection.ondatachannel = function (event) {
+				sendMsgBtn.disabled = false;
+				chatArea.style.display = '';
+				chatInput.style.display = '';
+				console.log('dataChannel opened');
+				dataChannel = event.channel;
+		
+				dataChannel.onmessage = function (event) { 
+					chatArea.innerHTML += connectedUser + ": " + event.data + "<br />";
+					chatArea.scrollTop = chatArea.scrollHeight;
+				};
+
+				dataChannel.onerror = function (error) { 
+					console.log("Ooops...error:", error); 
+				};
+
+				dataChannel.onclose = dataChannelOnClose;
+			};
+			chatBtn.disabled = true;
+			break;
+		
+		case 'file':
+			break;
+
+		default:
+			console.error(`Unknown offer.app: ${offer.ap}`);
+	}
+
+	// 144
+	peerConnection.createAnswer(async function (answer) { 
+		await peerConnection.setLocalDescription(answer); 
+		console.log('createAnswer setLocalDescription');
+		ws.send({ type: "answer", answer: answer });
+		handleCandidate();
+	}, function (error) { 
+		alert("Error when creating an answer"); 
+	});
+
+	hangUpBtn.disabled = false;
+};
+ 
+// Handle WebRTC answer 
+async function handleAnswer(answer) { 
+	await peerConnection.setRemoteDescription(answer); //(new RTCSessionDescription(answer));
+	remoteDescriptionSet = true;
+	handleCandidate();
+	console.log('answer setRemoteDescription');
+	sendMsgBtn.disabled = false;
+};
+ 
+// Handle WebRTC ice candidate 
+// If candidate undefined this is call to handle any buffered candidates
+async function handleCandidate(candidate) {
+	//if(peerConnection) {
+	if(remoteDescriptionSet) {
+		// handle any buffered candidates then this one, if present
+		if(candidate) iceBuffer.push(candidate);
+		let entry = null;
+		while((entry = iceBuffer.shift())) {
+			try {
+				console.log(`current remote description: ${peerConnection.currentRemoteDescription != null ? true : false}`);
+				await peerConnection.addIceCandidate(entry);
+			} catch(e) {
+				console.error(`peerConnection.addIceCandidate error: ${e}`);
+			}
+		}
+	} else {
+		if(candidate) iceBuffer.push(candidate); // should always be a candidate
+	}
+	//};
+};
---
+++ video.js
@@ -0,0 +1,405 @@
+'use strict';
+
+/***** Globals *****/
+
+let otherUsername = null;
+let connection = null;
+let sendMessage = null;
+let localStream = null;
+
+/***** UI *****/
+
+const divLogin = document.querySelector('div#login');
+const loginStatus = document.querySelector('#loginStatus');
+const LoginPage = {
+	Form: {
+		Email: document.getElementById('LoginPage.Form.Email'),
+		RememberMeChkBx: document.getElementById('LoginPage.Form.RememberMeCkhBx'),
+		PasswordDiv: document.getElementById('LoginPage.Form.PasswordDiv'),
+		Password: document.getElementById('LoginPage.Form.Password'),
+		SubmitLogin: document.getElementById('LoginPage.Form.SubmitLogin')
+	}
+};
+
+const divCall = document.querySelector('div#call');
+const userNameToCall = document.querySelector('input#username-to-call');
+const callButton = document.querySelector('button#call');
+const closeButton = document.querySelector('button#close-call');
+const brumeName = document.querySelector('#brume_name');
+const localDiv = document.getElementById('localDiv');
+const localButton = document.querySelector('button.local');
+const remoteDiv = document.getElementById('remoteDiv');
+const remoteButton = document.querySelector('button.remote');
+
+remoteDiv.style.visibility = 'hidden';
+localDiv.style.visibility = 'hidden';
+localButton.style.textDecoration = 'line-through';
+remoteButton.style.textDecoration = 'line-through';
+divCall.style.display = 'none';
+
+/***** Websocket *****/
+
+function wsConnect(url) {
+	return new Promise((res, rej) => {
+		let ws = new WebSocket(url);
+
+		ws.onopen = () => {
+			console.log('Connected to the signaling server');
+			res(sendMessage);
+		};
+
+		ws.onclose = () => { 
+			ws = null;
+		};
+
+		ws.onerror = err => {
+			alert(`wsconnect error: ${JSON.stringify(err)}`);
+			console.error(`ws.onerror: ${JSON.stringify(err)}`);
+			rej(null);
+		};
+
+		ws.onmessage = msg => {
+			const data = JSON.parse(msg.data);
+			const type = data.type == 'peerError' ? data.type : data.data.type;
+			console.log('Got message', type);
+	
+			switch (type) {
+				case 'offer':
+					handleOffer(data.data.offer, data.from);
+					break;
+	
+				case 'answer':
+					handleAnswer(data.data.answer);
+					break;
+	
+				case 'candidate':
+					handleCandidate(data.data.candidate);
+					break;
+
+				case 'close':
+					handleClose();
+					break;
+
+				case 'peerError':
+					switch(data.code) {
+						case 'ENODEST':
+							alert(`${data.edata.receiver} is not connected`);
+							break;
+	
+						default:
+							alert(`peerError: ${data.data}`);
+					}
+					handleClose();
+					break;
+	
+				default:
+					break;
+			}
+		};
+
+		function sendMessage(message){
+			/*if (otherUsername) {
+				message.name = otherUsername;
+			}*/
+
+			if(ws == null) {
+				return false;
+			}
+		
+			ws.send(JSON.stringify({
+				action: 'send',
+				to: otherUsername,
+				data: {
+					...message
+				}
+			}));
+			return true;
+		}
+	});
+}
+
+/***** Brume login *****/
+
+const CLIENTID = '6dspdoqn9q00f0v42c12qvkh5l';
+const REGION = 'us-east-1';
+const cognito = new AWS.CognitoIdentityServiceProvider({region : REGION});
+
+function doToggle(event) {
+	// toggle the type attribute
+	let password = document.getElementById(event.currentTarget.attributes.getNamedItem('for').value);
+	password.type = password.type === "password" ? "text" : "password";
+	// toggle the icon
+	event.currentTarget.classList.toggle("bi-eye");
+}
+
+async function loggedIn(token) {
+	let username = JSON.parse(atob(token.split('.')[1]))['custom:brume_name'];
+	try {
+		sendMessage = await wsConnect(`${location.protocol == 'https:' ? 'wss' : 'ws'}://${location.host}?token=${token}`);
+		divLogin.style.display = 'none';
+		divCall.style.display = 'block';
+		alert(`Signed in as ${username}`);
+	} catch(e) {
+		alert(`Error connecting to Brume server: ${location.protocol == 'https:' ? 'wss' : 'ws'}://${location.host}?token= ...token`);
+		delete localStorage.Authorization;
+	}
+}
+
+function processLogin() {
+	if (LoginPage.Form.RememberMeChkBx.checked && LoginPage.Form.Email.value !== "") {
+		localStorage.email = LoginPage.Form.Email.value;
+		localStorage.checkbox = LoginPage.Form.RememberMeChkBx.value;
+	} else {
+		localStorage.email = "";
+		localStorage.checkbox = "";
+	}
+
+	const params = {
+		AuthFlow: "USER_PASSWORD_AUTH",
+		ClientId: CLIENTID,
+		AuthParameters : {
+			USERNAME: LoginPage.Form.Email.value,
+			PASSWORD: LoginPage.Form.Password.value
+		}
+	};
+
+	cognito.initiateAuth(params, function(err,data) {
+		if (err) {
+			// Login fail
+			if(err.code == 'NotAuthorizedException') {
+				loginStatus.innerHTML = err.message;
+			}
+			delete localStorage.Authorization;
+		} else {
+			// Login success
+			if(data.ChallengeName && data.ChallengeName == "NEW_PASSWORD_REQUIRED"){
+				alert('New Password Required. Change your password at brume.occams.solutions.');
+			} else {
+				localStorage.Authorization = data.AuthenticationResult.IdToken;
+				loggedIn(localStorage.Authorization);
+			}
+		}
+	});
+}
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+	LoginPage.Form.RememberMeChkBx.setAttribute("checked", "checked");
+	LoginPage.Form.Email.value = localStorage.email;
+} else {
+	LoginPage.Form.RememberMeChkBx.removeAttribute("checked");
+	LoginPage.Form.Email.value = "";
+}
+
+if(localStorage.Authorization && localStorage.Authorization != '') {
+	if(new Date(JSON.parse(atob(localStorage.Authorization.split('.')[1])).exp * 1000) < new Date()) {
+		loggedIn(localStorage.Authorization);
+	} else {
+		delete localStorage.Authorization;
+	}
+}
+
+/***** WebRTC *****/
+
+const handleLogin = async success => {
+	if (success === false) {
+		alert('😞 Username already taken');
+	} else {
+		if(localStream == null){
+			try {
+				localStream = await navigator.mediaDevices.getUserMedia({
+					video: true,
+					audio: true
+				});
+			} catch (error) {
+				alert(`navigator.mediaDevices.getUserMedia: ${error.name}`);
+				console.error(`navigator.mediaDevices.getUserMedia: ${JSON.stringify(error)}`);
+			}
+
+			localStream.getTracks().forEach(media => {
+				media.enabled = false;
+			});
+
+			document.querySelector('video#local').srcObject = localStream;
+		}
+		console.log(`create connection`);
+		connection = new RTCPeerConnection({
+			iceServers: [{ url: 'stun:stun2.1.google.com:19302' }]
+		});
+
+		console.log(`created connection`);
+
+		connection.onclose = e=> {
+			handleClose();
+		};
+
+		connection. onconnectionstatechange = () => {
+			console.log(`connectionState: ${connection.connectionState}`);
+			if (["disconnected", "failed", "closed"].includes(connection.connectionState)) {
+				console.log(`remote closed connection`);
+				handleClose();
+			}
+		};
+
+		connection.addStream(localStream);
+
+		connection.onaddstream = event => {
+			document.querySelector('video#remote').srcObject = event.stream;
+		};
+
+		connection.onicecandidate = event => {
+			if (event.candidate) {
+				sendMessage({
+					type: 'candidate',
+					candidate: event.candidate
+				});
+			}
+		};
+	}
+};
+
+callButton.addEventListener('click', async function() {
+	otherUsername = document.querySelector('input#username-to-call').value;
+
+	if (otherUsername.length === 0) {
+		alert('Enter a username 😉');
+		return;
+	}
+
+	await handleLogin(true);
+	localDiv.style.visibility = '';
+	remoteButton.innerHTML = `${otherUsername}'s video`;
+
+	connection.createOffer(
+		offer => {
+			sendMessage({
+				type: 'offer',
+				offer: offer
+			});
+
+			connection.setLocalDescription(offer);
+			console.log('createOffer setLocalDescription');
+		},
+		error => {
+			alert('Error when creating an offer');
+			console.error(`createOffer error: ${JSON.stringify(error)}`);
+		}
+	);
+});
+
+const handleOffer = async (offer, username) => {
+	await handleLogin(true);
+	otherUsername = username;
+	localDiv.style.visibility = '';
+	remoteDiv.style.visibility = '';
+	remoteButton.innerHTML = `${otherUsername}'s video`;
+
+	connection.setRemoteDescription(offer);
+	connection.createAnswer(
+		answer => {
+			connection.setLocalDescription(answer);
+			console.log('createAnswer setLocalDescription');
+			sendMessage({
+				type: 'answer',
+				name: username, //to work with chat server
+				answer: answer
+			});
+		},
+		error => {
+			alert('Error when creating an answer');
+			console.error(`createAnswer error: ${JSON.stringify(error)}`);
+		}
+	);
+	callButton.classList.toggle('hidden');
+	closeButton.classList.toggle('hidden');
+	userNameToCall.classList.toggle('hidden');
+	console.log(`sent answer`);
+};
+
+const handleAnswer = answer => {
+	connection.setRemoteDescription(answer);
+	remoteDiv.style.visibility = '';
+	callButton.classList.toggle('hidden');
+	closeButton.classList.toggle('hidden');
+	userNameToCall.classList.toggle('hidden');
+};
+
+const handleCandidate = candidate => {
+	//console.log(`current remote description: ${connection.currentRemoteDescription != null ? true : false}`);
+	connection.addIceCandidate(candidate); //(new RTCIceCandidate(candidate));
+};
+
+document.querySelector('button#close-call').addEventListener('click', () => {
+	sendMessage({
+		type: 'close'
+	});
+
+	handleClose();
+});
+
+const handleClose = () => {
+	console.log('closing connection');
+	otherUsername = null;
+	localStream.getTracks().forEach(media => { media.enabled = false; });
+
+	// 10/6
+	localStream.getVideoTracks()[0].stop();
+	localStream = null;
+	firstUserGesture = true;
+
+	//const video = document.querySelector('video#remote'); 10/6
+	Array.from(document.getElementsByTagName('video')).forEach(video => {
+		video.pause();
+		video.src = '';
+		video.srcObject = null;  
+		video.load();
+	});
+
+	callButton.classList.toggle('hidden');
+	closeButton.classList.toggle('hidden');
+	userNameToCall.classList.toggle('hidden');
+	localButton.style.textDecoration = 'line-through';
+	localDiv.style.visibility = 'hidden';
+	remoteButton.innerHTML = 'Remote video';
+	remoteButton.style.textDecoration = 'line-through';
+	remoteDiv.style.visibility = 'hidden';
+	//initialize label of remote button
+	connection.close();
+	connection = null;
+};
+
+let firstUserGesture = true;
+function toggle(buttonName) {
+	const button = document.querySelector('#' + buttonName + 'Button');
+	const player = document.querySelector('video#'+buttonName);
+
+	if(buttonName == 'local') {
+		if(firstUserGesture) {
+			player.play();
+			button.style.textDecoration = '';
+			firstUserGesture = false;
+
+			localStream.getTracks().forEach(media => {
+				media.enabled = true;
+			});
+
+			return;
+		} else {
+			button.style.textDecoration = button.style.textDecoration == 'line-through' ? '' : 'line-through';
+			localStream.getTracks().forEach(media => {
+				media.enabled = media.enabled ? false : true;
+			});
+		}
+	} else {
+		if (player.paused) {
+			player.play();
+			player.muted = false;
+			button.style.textDecoration = '';
+		} else {
+			player.src='';
+			player.pause();
+			player.muted = true;
+			button.style.textDecoration = 'line-through';
+		}
+	}
+};
+
