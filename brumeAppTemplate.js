8361371dfa16642920b05fe3b8d83004f0a5632c
Bob <blund@cablelabs.com>
2022-10-16 07:28:41 -0600

    Basic Brume web app template

---
+++ brumeAppTemplate.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import {wsConnect} from './brumeWebsocket.js';
+import {getToken} from './brumeLogin.js';
+
+const wsRoutes = {};
+let sendMessage = null;
+let token = null;
+
+
+const divLogin = document.querySelector('div#login');
+const divApp = document.querySelector('div#app');
+
+divLogin.classList.add('hidden');
+divApp.classList.add('hidden');
+token = getToken(loginCallBack);
+
+if(token != null) {
+	divLogin.classList.add('hidden');
+	divApp.classList.remove('hidden');
+} else {
+	divLogin.classList.remove('hidden');
+}
+
+function loginCallBack(brumeToken) {
+	token = brumeToken;
+	divLogin.classList.add('hidden');
+	divApp.classList.remove('hidden');
+};
+
+// Do app stuff
+
+document.querySelector('#startAppBtn').addEventListener('click', async (e) => {
+	try {	
+		sendMessage = await wsConnect(token, wsRoutes);
+		alert(`Connected to brume server`);
+		document.querySelector('#startAppBtn').classList.add('hidden');
+	} catch(e) {
+		alert(`Cannot connect to Brume: ${e}`);
+	}
+});
