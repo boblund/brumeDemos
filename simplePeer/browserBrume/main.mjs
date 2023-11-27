98aa273fed1294d0c5c0ce4b4f63e59da834c52a
Bob <bob.lund@gmail.com>
2023-11-27 15:37:12 -0700

    remove peerEmit()

--- simplePeer/browserBrume/main.mjs
+++ simplePeer/browserBrume/main.mjs
@@ -6,31 +6,6 @@ import {getToken} from './brumeLogin.mjs';
 const brume = new Brume(),
 	callElem = customElements.get('brume-call') ? document.getElementById('call') : null;
 
-/*****App specific handling of peer connection events *****/
-
-function peerInit(peer, peerName) {
-	peer.on('closed', () => {
-		console.log(`peer closed`);
-		handleClose();
-	});
-
-	peer.on('peerError', (data) => {
-		switch(data.code) {
-			case 'ENODEST':
-				alert(`${data.peerUsername} is not connected`);
-				break;
-
-			case 'EOFFERTIMEOUT':
-				alert(`${data.peerUsername} did not answer`);
-				break;	
-	
-			default:
-				alert(`peerError: ${data}`);
-		}
-		handleClose();
-	});
-}
-
 /***** App specific handling of Brume signaling events *****/
 
 let peer = null;
@@ -39,7 +14,6 @@ function offerHandler(peer) {
 	if(confirm(`Accept call from ${peer.peerUsername}?`)){
 		callElem.name.value = `call from ${peer.peerUsername}`;
 		//peer = await brumeConnection.makePeer({channelId});
-		peerInit(peer);
 		callElem.hangUpBtn.addEventListener("click", () => { hangupHandler(peer); });
 		callElem.hangUp();
 		peer.on('close', () => {
@@ -77,7 +51,6 @@ callElem.callBtn.addEventListener('click', async (e) => {
 		peer.on('close', () => {
 			handleClose();
 		});
-		peerInit(peer, callElem.name.value);
 		callElem.hangUpBtn.addEventListener("click", () => { hangupHandler(peer); });
 		callElem.hangUp();
 		console.log(`caller data channel open`);
